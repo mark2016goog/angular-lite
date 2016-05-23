4240cc7bc4df6f1d3bdf9c520ce2e4d0689ff2e0
shengxinjing <i@shengxinjing.cn>
2016-05-24 07:17:02 +0800

    test

--- tests/scope_spec.js
+++ tests/scope_spec.js
@@ -1467,13 +1467,11 @@ describe('Scope', () => {
 
 	})
 
-
-
 	it('receive by listeners on current scope after being stopped',()=>{
 		let listener1 = event=>{
 			event.stopPropagation()
 		}
-		let listener2 = jasmine.createSpy()
+		let 5tlistener2 = jasmine.createSpy()
 		scope.$on('someEvent', listener1)
 		scope.$on('someEvent', listener2)
 
